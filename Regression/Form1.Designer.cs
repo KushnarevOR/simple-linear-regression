99d51cb2e48eb1087e73adddbff2b0d9a95a7a91
Олег Кушнарев <oleg.kushnarev.99@mail.ru>
2021-08-06 16:54:17 +1000

    Добавьте файлы проекта.

---
+++ Regression/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Regression
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Обзор";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Обзор";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(87, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Имя текстового файла";
+            this.label1.TextChanged += new System.EventHandler(this.label1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(87, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Имя текстового файла";
+            this.label2.TextChanged += new System.EventHandler(this.label2_TextChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(547, 59);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Признак-фактор";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(12, 77);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(547, 59);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Результативный признак";
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(484, 321);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Execute";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 143);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(547, 172);
+            this.textBox1.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(571, 350);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Regression";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Regression/Form1.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Regression
+{
+    public partial class Form1 : Form
+    {
+        List<float> X_var;
+        List<float> Y_var;
+        List<float> Eq;
+        List<float> F_tab;
+        int obs_num;
+        float X_dev;
+        float Y_dev;
+        float error;
+        float F;
+        float R2;
+        float uh;//коэффициент эластичности
+        float a;
+        float b;
+        float t_a;
+        float t_b;
+   
+        public Form1()
+        {
+            InitializeComponent();
+            X_var = new List<float>();
+            Y_var = new List<float>();
+            Eq = new List<float>();
+            F_tab = new List<float>() { 161.45f, 199.50f, 215.71f, 224.58f, 230.16f, 233.99f, 236.77f, 238.88f, 240.54f, 241.88f, 245.95f };
+        }
+
+        public float calc_t_a(List<float> x_var, List<float> y_var, List<float> eq, float x_dev, float a)
+        {
+            float S = 0;
+            float x2_sum = 0;
+            for (int i = 0; i < y_var.Count(); i++)
+            {
+                S += (float)Math.Pow((y_var[i] - eq[i]), 2);
+                x2_sum += (float)Math.Pow(x_var[i], 2);
+            }
+            S = (float)Math.Sqrt(S / y_var.Count()
+                );
+
+            float ma = (S * (float)Math.Sqrt(x2_sum)) / (y_var.Count() * x_dev);
+
+            return a / ma;
+        }
+
+        public float calc_t_b(List<float> y_var, List<float> eq, float x_dev, float b)
+        {
+            float S = 0;
+            for (int i = 0; i < y_var.Count(); i++)
+            {
+                S += (float)Math.Pow((eq[i] - y_var[i]), 2);
+            }
+            S = (float)Math.Sqrt(S / y_var.Count());
+
+            float mb = S / (x_dev * (float)Math.Sqrt(y_var.Count()));
+
+            return b / mb;
+        }
+
+        public List<float> calc_eq(List<float> x_var, float a, float b)
+        {
+            List<float> eq = new List<float>();
+            foreach (var item in x_var)
+                eq.Add(a + b * item);
+
+            return eq;
+        }
+
+        public float calc_err(List<float> y_var, List<float> eq)
+        {
+            float sum = 0;
+            for(int i = 0; i < y_var.Count(); i++)
+            {
+                sum += Math.Abs((y_var[i] - eq[i]) / y_var[i]) * 100;
+            }
+
+            return sum / y_var.Count();
+        }
+
+        public float calc_F(float x_dev, float y_dev, float b)
+        {
+            float r2 = (float)Math.Pow((b * x_dev / y_dev), 2);
+
+            return (X_var.Count - 2) * r2 / (1 - r2);
+        }
+
+        public float calc_R(List<float> y_var, List<float> eq)
+        {
+            float sum_1 = 0;
+            float sum_2 = 0;
+            for(int i = 0; i < y_var.Count; i++)
+            {
+                sum_1 += (float)Math.Pow((eq[i] - y_var.Average()), 2);
+                sum_2 += (float)Math.Pow((y_var[i] - y_var.Average()), 2);
+            }
+
+            return sum_1 / sum_2;
+        }
+
+        public float calc_uh(List<float> x_var, List<float> y_var, float b)
+        {
+            return b * x_var.Average() / y_var.Average();
+        }
+
+        public float calc_dev_X(List<float> x_var)
+        {
+            float sum = 0;
+            for(int i = 0; i < x_var.Count; i++)
+            {
+                sum += (float)Math.Pow((x_var[i] - x_var.Average()), 2);
+            }
+            return (float)Math.Sqrt(sum / x_var.Count());
+        }
+
+        public float calc_dev_Y(List<float> y_var)
+        {
+            float sum = 0;
+            for (int i = 0; i < y_var.Count; i++)
+            {
+                sum += (float)Math.Pow((y_var[i] - y_var.Average()), 2);
+            }
+            return (float)Math.Sqrt(sum / y_var.Count());
+        }
+
+        public float calc_b(List<float> x_var, List<float> y_var)
+        {
+            List<float> yx = new List<float>();
+            List<float> xx = new List<float>();
+            
+            for(int i = 0; i < x_var.Count; i++)
+                yx.Add(x_var[i] * y_var[i]);
+            foreach (var item in x_var)
+                xx.Add((float)Math.Pow(item, 2));
+
+            return (yx.Average() - y_var.Average() * x_var.Average()) / (xx.Average() - (float)Math.Pow(x_var.Average(), 2));
+        }
+
+        public float calc_a(List<float> x_var, List<float> y_var, float b)
+        {
+            return (y_var.Average() - b * x_var.Average());
+        }
+
+        private void label1_TextChanged(object sender, EventArgs e)
+        {
+            if (label1.Text != "Имя текстового файла" && label2.Text != "Имя текстового файла")
+                button3.Enabled = true;
+        }
+
+        private void label2_TextChanged(object sender, EventArgs e)
+        {
+            if (label1.Text != "Имя текстового файла" && label2.Text != "Имя текстового файла")
+                button3.Enabled = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string num;
+            OpenFileDialog file = new OpenFileDialog();
+            file.InitialDirectory = @"C:\Users\Olga\Desktop\Многомерный статистический анализ\лаб4";
+            file.Filter = "txt files (*.txt)|*.txt";
+            file.Title = "Factor";
+            if(file.ShowDialog() == DialogResult.OK)
+            {
+                X_var.Clear();
+                string filepath = file.FileName;
+                label1.Text = filepath;
+                using (StreamReader reader = new StreamReader(filepath))
+                {
+                    try
+                    {
+                        while ((num = reader.ReadLine()) != null)
+                        {
+                            X_var.Add(float.Parse(num));
+                        }
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Something went wrong!");
+                        X_var.Clear();
+                    }
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string num;
+            OpenFileDialog file = new OpenFileDialog();
+            file.InitialDirectory = @"C:\Users\Olga\Desktop\Многомерный статистический анализ\лаб4";
+            file.Filter = "txt files (*.txt)|*.txt";
+            file.Title = "Result";
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                Y_var.Clear();
+                string filepath = file.FileName;
+                label2.Text = filepath;
+                using (StreamReader reader = new StreamReader(filepath))
+                {
+                    try
+                    {
+                        while ((num = reader.ReadLine()) != null)
+                        {
+                            Y_var.Add(float.Parse(num));
+                        }
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Something went wrong!");
+                        Y_var.Clear();
+                    }
+                }
+            }
+        }
+
+        private void check_the_model(float R, float error, float F, float a, float b)
+        {
+            bool check = true;
+            if (R >= 0.7 && error <= 10)
+                textBox1.Text += "Модель высокого качества\r\n";
+            else
+            {
+                textBox1.Text += "Модель низкого качества\r\n";
+                check = false;
+            }
+
+            if (obs_num > F_tab.Count())
+                obs_num = F_tab.Count();
+
+            if (F >= F_tab[obs_num - 1])
+                textBox1.Text += "Оцениваемые характеристики статистически значимы и надежны\r\n";
+            else
+            {
+                textBox1.Text += "Оцениваемые характеристики ненадежны\r\n";
+                check = false;
+            }
+
+            if (!check)
+                textBox1.Text += "\r\nПо данной модели нельзя строить прогнозы!";
+            else
+            {
+                float eq = a + b * (X_var.Average() * 1.1f);
+                textBox1.Text += "\r\nПрогноз при увеличении X на 10%: Y = " + eq;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            b = calc_b(X_var, Y_var);
+            a = calc_a(X_var, Y_var, b);
+            Eq = calc_eq(X_var, a, b);
+            error = calc_err(Y_var, Eq);
+            X_dev = calc_dev_X(X_var);
+            Y_dev = calc_dev_Y(Y_var);
+            F = calc_F(X_dev, Y_dev, b);
+            R2 = calc_R(Y_var, Eq);
+            uh = calc_uh(X_var, Y_var, b);
+            obs_num = X_var.Count();
+            t_a = calc_t_a(X_var, Y_var, Eq, X_dev, a);
+            t_b = calc_t_b(Y_var, Eq, X_dev, b);
+            textBox1.Text += "Уравнение регрессии: Y = " + a + " + " + b + "*X" + "\r\n";
+            textBox1.Text += "Средняя ошибка аппроксимации: A = " + error + "%" + "\r\n";
+            textBox1.Text += "Коэффициент детерминации: R_квадрат = " + R2 + "\r\n";
+            textBox1.Text += "Средний коэффициент эластичности Э = " + uh + "\r\n";
+            textBox1.Text += "Критерий Фишера F = " + F + "\r\n";
+            textBox1.Text += "________________________________________________________________________\r\n";
+            check_the_model(R2, error, F, a, b);
+        }
+    }
+}
